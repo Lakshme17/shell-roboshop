197e0fbf2d29af63a880db7a3022cc56012a3c56
siva <tandrusaichowdary@gmail.com>
2025-10-08 08:39:57 +0530

    message

--- roboshop.sh
+++ roboshop.sh
@@ -6,7 +6,7 @@ SG_ID="sg-0789ef71317d7634f" #REPLACE WITH YOUR sg ID
 
 for Instance in $@ #dynamically 
 do
-      INSTANCE=$*(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-0789ef71317d7634f --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceId' --output text)
+      INSTANCE=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-0789ef71317d7634f --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceId' --output text)
 
       #get private ip
       if [ $instance != "frontend" ]: then
