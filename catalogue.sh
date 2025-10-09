7470c7f655e5f381873038bdcd7c429eca5e1e90
siva <tandrusaichowdary@gmail.com>
2025-10-09 23:54:04 +0530

    message

--- catalogue.sh
+++ catalogue.sh
@@ -85,7 +85,7 @@ dnf install mongodb-mongosh -y &>>$LOG_FILE
 VALIDATE $? "installing MongoDB Client"
 
 INDEX=$(mongosh $MONGODB_HOST --quiet --eval "db.getMongo().getDBNames().indexOf('catalogue')")
-if [ $INDEX -le 0 ]; then
+if [ "$INDEX" -le 0 ]; then
     mongosh --host $MONGODB_HOST </app/db/master-data.js &>>$LOG_FILE
     VALIDATE $? "Load catalogue products"
 else
