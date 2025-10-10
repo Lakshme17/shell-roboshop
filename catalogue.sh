fd26330daa4109f8b0e3374607dcb13bf8776c9c
siva <tandrusaichowdary@gmail.com>
2025-10-10 11:17:04 +0530

    message

--- catalogue.sh
+++ catalogue.sh
@@ -1,49 +1,55 @@
 #!/bin/bash
 
-USERID=$(id -u)
-R="\e[31m"
-G="\e[32m"
-Y="\e[33m"
-N="\e[0m"
+# Define color codes
+R="\033[31m"
+G="\033[32m"
+Y="\033[33m"
+N="\033[0m"
 
+# Check if script is run as root
+USERID=$(id -u)
+if [ "$USERID" -ne 0 ]; then
+    echo -e "${R}Error:${N} Please run this script with root privileges"
+    exit 1
+fi
+
+# Setup logging
 LOGS_FOLDER="/var/log/shell-roboshop"
 SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
 SCRIPT_DIR=$PWD
 MONGODB_HOST=mongodb.lakshme.website
-LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" # /var/log/shell-script/16-logs.log
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
 
-mkdir -p $LOGS_FOLDER
-echo "Script started executed at: $(date)" | tee -a $LOG_FILE
+mkdir -p "$LOGS_FOLDER"
+echo "Script started at: $(date)" | tee -a "$LOG_FILE"
 
-if [ $USERID -ne 0 ]; then
-    echo "ERROR:: Please run this script with root privelege"
-    exit 1 # failure is other than 0
-fi
-
-VALIDATE(){ # functions receive inputs through args just like shell script args
-    if [ $1 -ne 0 ]; then
-        echo -e "$2 ... $R FAILURE $N" | tee -a $LOG_FILE
+# Validation function
+VALIDATE() {
+    if [ "$1" -ne 0 ]; then
+        echo -e "$2 ....... ${R}Failure${N}" | tee -a "$LOG_FILE"
         exit 1
     else
-        echo -e "$2 ... $G SUCCESS $N" | tee -a $LOG_FILE
-    fi
+        echo -e "$2 ....... ${G}Success${N}" | tee -a "$LOG_FILE"
+    fi        
 }
 
-##### NodeJS ####
+#####Installing Catalogue ######NodeJS
 dnf module disable nodejs -y &>>$LOG_FILE
-VALIDATE $? "Disabling NodeJS"
-dnf module enable nodejs:20 -y  &>>$LOG_FILE
+VALIDATE $? "Disabling the NodeJS"
+
+dnf module enable nodejs:20 -y &>>$LOG_FILE
 VALIDATE $? "Enabling NodeJS 20"
+
 dnf install nodejs -y &>>$LOG_FILE
 VALIDATE $? "Installing NodeJS"
 
-id roboshop &>>$LOG_FILE
+id roboshop
 if [ $? -ne 0 ]; then
-    useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOG_FILE
-    VALIDATE $? "Creating system user"
+     useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOG_FILE
+     VALIDATE $? "Creating system user"
 else
-    echo -e "User already exist ... $Y SKIPPING $N"
-fi
+     echo -e "User already exits ....$Y SKIPPING $N"
+fi     
 
 mkdir -p /app
 VALIDATE $? "Creating app directory"
@@ -51,8 +57,8 @@ VALIDATE $? "Creating app directory"
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip &>>$LOG_FILE
 VALIDATE $? "Downloading catalogue application"
 
-cd /app 
-VALIDATE $? "Changing to app directory"
+cd  /app
+VALIDATE $? "Changing the app directory"
 
 rm -rf /app/*
 VALIDATE $? "Removing existing code"
@@ -61,9 +67,11 @@ unzip /tmp/catalogue.zip &>>$LOG_FILE
 VALIDATE $? "unzip catalogue"
 
 npm install &>>$LOG_FILE
-VALIDATE $? "Install dependencies"
+VALIDATE $? "Install dependencies "
 
-cp $SCRIPT_DIR/catalogue.service /etc/systemd/system/catalogue.service
+
+
+cp $SCRIPT_DIR/catalogue.service  /etc/systemd/system/catalogue.service
 VALIDATE $? "Copy systemctl service"
 
 systemctl daemon-reload
@@ -74,15 +82,19 @@ cp $SCRIPT_DIR/mongo.repo /etc/yum.repos.d/mongo.repo
 VALIDATE $? "Copy mongo repo"
 
 dnf install mongodb-mongosh -y &>>$LOG_FILE
-VALIDATE $? "Install MongoDB client"
+VALIDATE $? "installing MongoDB Client"
 
-INDEX=$(mongosh mongodb.lakshme.website --quiet --eval "db.getMongo().getDBNames().indexOf('catalogue')")
+
+INDEX=$(mongosh $MONGODB_HOST --quiet --eval "db.getMongo().getDBNames().indexOf('catalogue')")
 if [ $INDEX -le 0 ]; then
     mongosh --host $MONGODB_HOST </app/db/master-data.js &>>$LOG_FILE
     VALIDATE $? "Load catalogue products"
 else
-    echo -e "Catalogue products already loaded ... $Y SKIPPING $N"
-fi
+     echo -e "Catalogue products already loaded ....$Y SKIPPING $N"
+fi     
+
+# mongosh --host $MONGODB_HOST </app/db/master-data.js
+# VALIDATE $? "Load catalogue products"
 
 systemctl restart catalogue
 VALIDATE $? "Restarted catalogue"
