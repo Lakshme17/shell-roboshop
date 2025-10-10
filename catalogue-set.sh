e5c5744bdd48b37299061dc116473cde1ef552f9
siva <tandrusaichowdary@gmail.com>
2025-10-10 13:06:10 +0530

    message

---
+++ catalogue-set.sh
@@ -0,0 +1,109 @@
+#!/bin/bash
+
+set -euo pipefail
+
+trap 'echo "There is an error in $LINENO, Command is: $BASH_COMMAND"' ERR
+
+# Define color codes
+R="\033[31m"
+G="\033[32m"
+Y="\033[33m"
+N="\033[0m"
+
+# Check if script is run as root
+USERID=$(id -u)
+if [ "$USERID" -ne 0 ]; then
+    echo -e "${R}Error:${N} Please run this script with root privileges"
+    exit 1
+fi
+
+# Setup logging
+LOGS_FOLDER="/var/log/shell-roboshop"
+SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
+SCRIPT_DIR=$PWD
+MONGODB_HOST=mongodb.lakshme.website
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+
+mkdir -p "$LOGS_FOLDER"
+echo "Script started at: $(date)" | tee -a "$LOG_FILE"
+
+# Validation function
+# VALIDATE() {
+#     if [ "$1" -ne 0 ]; then
+#         echo -e "$2 ....... ${R}Failure${N}" | tee -a "$LOG_FILE"
+#         exit 1
+#     else
+#         echo -e "$2 ....... ${G}Success${N}" | tee -a "$LOG_FILE"
+#     fi        
+# }
+
+#####Installing Catalogue ######NodeJS
+dnf module disable nodejs -y &>>$LOG_FILE
+# VALIDATE $? "Disabling the NodeJS"
+
+dnf module enable nodejs:20 -y &>>$LOG_FILE
+# VALIDATE $? "Enabling NodeJS 20"
+
+dnf install nodejs -y &>>$LOG_FILE
+# VALIDATE $? "Installing NodeJS"
+echo -e "Installing NodeJS 20...... $G SUCCESS $N"
+id roboshop
+if [ $? -ne 0 ]; then
+     useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOG_FILE
+    #  VALIDATE $? "Creating system user"
+else
+     echo -e "User already exits ....$Y SKIPPING $N"
+fi     
+
+mkdir -p /app
+# VALIDATE $? "Creating app directory"
+
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip &>>$LOG_FILE
+# VALIDATE $? "Downloading catalogue application"
+
+cd  /app
+# VALIDATE $? "Changing the app directory"
+
+rm -rf /app/*
+# VALIDATE $? "Removing existing code"
+
+unzip /tmp/catalogue.zip &>>$LOG_FILE
+# VALIDATE $? "unzip catalogue"
+
+npm install &>>$LOG_FILE
+# VALIDATE $? "Install dependencies"
+
+
+
+cp $SCRIPT_DIR/catalogue.service  /etc/systemd/system/catalogue.service
+# VALIDATE $? "Copy systemctl service"
+
+systemctl daemon-reload
+systemctl enable catalogue &>>$LOG_FILE
+# VALIDATE $? "Enable catalogue"
+
+echo -e "Catalogue set is completed....$G SUCCESS $N"
+
+cp $SCRIPT_DIR/mongo.repo /etc/yum.repos.d/mongo.repo
+# VALIDATE $? "Copy mongo repo"
+
+dnf install mongodb-mongosh -y &>>$LOG_FILE
+# VALIDATE $? "installing MongoDB Client"
+
+
+INDEX=$(mongosh $MONGODB_HOST --quiet --eval "db.getMongo().getDBNames().indexOf('catalogue')")
+if [ $INDEX -le 0 ]; then
+    mongosh --host $MONGODB_HOST </app/db/master-data.js &>>$LOG_FILE
+    # VALIDATE $? "Load catalogue products"
+else
+     echo -e "Catalogue products already loaded ....$Y SKIPPING $N"
+fi     
+
+# mongosh --host $MONGODB_HOST </app/db/master-data.js
+# VALIDATE $? "Load catalogue products"
+
+systemctl restart catalogue
+echo -e "Loading products and restarting catalogue.....$G SUCCESS $N"
+# VALIDATE $? "Restarted catalogue"
+
+
