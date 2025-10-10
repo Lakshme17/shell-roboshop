f3debf9848457a86d4712af1f377bde2bc06f0d5
siva <tandrusaichowdary@gmail.com>
2025-10-10 22:07:15 +0530

    message

--- frontend.sh
+++ frontend.sh
@@ -1,37 +1,32 @@
 #!/bin/bash
 
-# Define color codes
-R="\033[31m"
-G="\033[32m"
-Y="\033[33m"
-N="\033[0m"
-
-# Check if script is run as root
 USERID=$(id -u)
-if [ "$USERID" -ne 0 ]; then
-    echo -e "${R}Error:${N} Please run this script with root privileges"
-    exit 1
-fi
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
 
-# Setup logging
 LOGS_FOLDER="/var/log/shell-roboshop"
 SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
 SCRIPT_DIR=$PWD
-MONGODB_HOST=mongodb.lakshme.website
-MYSQL_HOST=mysql.lakshme.website
-LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+MONGODB_HOST=mongodb.daws86s.fun
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" # /var/log/shell-script/16-logs.log
 
-mkdir -p "$LOGS_FOLDER"
-echo "Script started at: $(date)" | tee -a "$LOG_FILE"
+mkdir -p $LOGS_FOLDER
+echo "Script started executed at: $(date)" | tee -a $LOG_FILE
 
-# Validation function
-VALIDATE() {
-    if [ "$1" -ne 0 ]; then
-        echo -e "$2 ....... ${R}Failure${N}" | tee -a "$LOG_FILE"
+if [ $USERID -ne 0 ]; then
+    echo "ERROR:: Please run this script with root privelege"
+    exit 1 # failure is other than 0
+fi
+
+VALIDATE(){ # functions receive inputs through args just like shell script args
+    if [ $1 -ne 0 ]; then
+        echo -e "$2 ... $R FAILURE $N" | tee -a $LOG_FILE
         exit 1
     else
-        echo -e "$2 ....... ${G}Success${N}" | tee -a "$LOG_FILE"
-    fi        
+        echo -e "$2 ... $G SUCCESS $N" | tee -a $LOG_FILE
+    fi
 }
 
 dnf module disable nginx -y &>>$LOG_FILE
@@ -39,21 +34,19 @@ dnf module enable nginx:1.24 -y &>>$LOG_FILE
 dnf install nginx -y &>>$LOG_FILE
 VALIDATE $? "Installing Nginx"
 
-systemctl enable nginx 
-VALIDATE $? "Enabling Nginx service"
-
-systemctl restart nginx &>>$LOG_FILE
-VALIDATE $? "Starting Nginx service"
+systemctl enable nginx  &>>$LOG_FILE
+systemctl start nginx 
+VALIDATE $? "Starting Nginx"
 
 rm -rf /usr/share/nginx/html/* 
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip &>>$LOG_FILE
-
 cd /usr/share/nginx/html 
 unzip /tmp/frontend.zip &>>$LOG_FILE
 VALIDATE $? "Downloading frontend"
 
 rm -rf /etc/nginx/nginx.conf
 cp $SCRIPT_DIR/nginx.conf /etc/nginx/nginx.conf
+VALIDATE $? "Copying nginx.conf"
 
-systemctl restart nginx  &>>$LOG_FILE
-VALIDATE $? "Restarting Nginx service"
+systemctl restart nginx 
+VALIDATE $? "Restarting Nginx"
