22c38d08412daafe98f5a306a2d9de87aa4991cd
siva <tandrusaichowdary@gmail.com>
2025-10-10 21:49:06 +0530

    message

--- frontend.sh
+++ frontend.sh
@@ -51,6 +51,7 @@ VALIDATE $? "Starting Nginx service"
 
 rm -rf /usr/share/nginx/html/* 
 
+
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip
 
 cd /usr/share/nginx/html 
